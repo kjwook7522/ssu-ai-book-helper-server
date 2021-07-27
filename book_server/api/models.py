4e4274d931ff406460d2c8a8c38e4e2acd06f017
kjwook7522 <kjwook7522@gmail.com>
2021-07-27 18:28:11 +0900

    Feat: User 모델, 시리얼라이저 생성

--- book_server/api/models.py
+++ book_server/api/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+class User(models.Model):
+  student_id = models.IntegerField(primary_key=True)
+  password = models.CharField(max_length=64, null=False)
+  name = models.CharField(max_length=8, null=False)
+  email = models.EmailField(max_length=20, null=False)
+  phone = models.CharField(max_length=20, null=False)
