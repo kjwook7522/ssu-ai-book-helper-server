f652d3c7dd45a17722f547065ec1df312621139c
kjwook7522 <kjwook7522@gmail.com>
2021-07-28 01:00:23 +0900

    Feat: auth handler 생성

--- book_server/api/urls.py
+++ book_server/api/urls.py
@@ -1,6 +1,6 @@
-from django.urls import path, include
+from django.urls import path
 from api import auth
 
 urlpatterns = [
-  path('user', auth.getUsers, name='get_users')
+  path('user', auth.getUsers, name='get_users'),
 ]
