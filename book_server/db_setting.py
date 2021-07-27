504edc00442de5157e0218eb4654521d023ad7e7
김재욱 <kjwook7522@gmail.com>
2021-07-27 13:38:53 +0900

    Feat: DB setting 추가

---
+++ book_server/db_setting.py
@@ -0,0 +1,10 @@
+DATABASES = {
+  'default': {
+    'ENGINE': 'django.db.backends.mysql',
+    'NAME': 'ssu-ai-book',
+    'USER': 'admin',
+    'PASSWORD': 'ssu20160354',
+    'HOST': 'ssu-ai-book-helper.czqfysfqk8vm.ap-northeast-2.rds.amazonaws.com',
+    'PORT': '3306'
+  }
+}
